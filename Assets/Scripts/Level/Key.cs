7bde8dcb0307242b9da402b86924b79a10711aab
VizCo1 <al394491@uji.es>
2023-11-22 15:18:38 +0100

    Assets update

--- Assets/Scripts/Level/Key.cs
+++ Assets/Scripts/Level/Key.cs
@@ -1,18 +1,17 @@
+using MoreMountains.Feedbacks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-
-    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private MMF_Player _MMFplayer;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerMovement _))
         {
-            _particleSystem.transform.position = new Vector3(transform.position.x, _particleSystem.transform.position.y, transform.position.z);
-            _particleSystem.Play();  
+            _MMFplayer?.PlayFeedbacks(transform.position);
             transform.position = Vector3.zero;
             QuadrantManager.Instance.ChangeRandomVariant();
             LevelGameManager.Instance.KeyPickedUp();
